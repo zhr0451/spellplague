4e85c78cb8ce029eb2bcafcc23b87b2e065c5d3a
zhr0451 <1samokhvalov.zs@gmail.com>
2025-06-14 14:40:29 +0300

    Quartz sync: Jun 14, 2025, 2:40 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,8 +8,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
-    pageTitleSuffix: "",
+    pageTitle: "Чума Заклинаний",
+    pageTitleSuffix: "– DnD кампания",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
